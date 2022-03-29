7e346ea0fd8fc465c55b8a9d49b3c47aa6fb15ff
Kate4ka007 <shlemena@mail.ru>
2022-03-29 11:58:54 +0300

    feat: add voices

--- words.js
+++ words.js
@@ -149,8 +149,7 @@ function prevWord() {
     }    
     numbersWord.innerHTML = `<p>${count +1} out of ${wordList.length}</p>`;       
     wordEn.innerHTML = `<p>${wordList[count][0]}</p>`;
-    wordRu.innerHTML = `<p>${wordList[count][1]}</p>`; 
-    
+    wordRu.innerHTML = `<p>${wordList[count][1]}</p>`;    
     
 }
 
@@ -159,7 +158,7 @@ function prevWord() {
 next.addEventListener('click', nextWord);
 prev.addEventListener('click', prevWord);
 
-
+/*
 const textEl = document.getElementById('text');
 const speakEl = document.getElementById('speak');
 
@@ -184,4 +183,68 @@ function speakText() {
   
  
   window.speechSynthesis.speak(utterance);
+}*/
+
+const textEl = document.getElementById('text');
+const voiceInEl = document.getElementById('voice');
+const pitchInEl = document.getElementById('pitch');
+const rateInEl = document.getElementById('rate');
+const volumeInEl = document.getElementById('volume');
+const pitchOutEl = document.querySelector('output[for="pitch"]');
+const rateOutEl = document.querySelector('output[for="rate"]');
+const volumeOutEl = document.querySelector('output[for="volume"]');
+const speakEl = document.getElementById('speak');
+
+// add UI event handlers
+pitchInEl.addEventListener('change', updateOutputs);
+rateInEl.addEventListener('change', updateOutputs);
+volumeInEl.addEventListener('change', updateOutputs);
+speakEl.addEventListener('click', speakText);
+
+// update voices immediately and whenever they are loaded
+updateVoices();
+window.speechSynthesis.onvoiceschanged = updateVoices;
+
+function updateOutputs() {
+  // display current values of all range inputs
+  pitchOutEl.textContent = pitchInEl.value;
+  rateOutEl.textContent = rateInEl.value;
+  volumeOutEl.textContent = volumeInEl.value;
 }
+
+function updateVoices() {
+  // add an option for each available voice that isn't already added
+  window.speechSynthesis.getVoices().forEach(voice => {
+    const isAlreadyAdded = [...voiceInEl.options].some(option => option.value === voice.voiceURI);
+    if (!isAlreadyAdded) {
+      const option = new Option(voice.name, voice.voiceURI, voice.default, voice.default);
+      voiceInEl.add(option);
+    }
+  });
+}
+
+function speakText() {
+  // stop any speaking in progress
+  window.speechSynthesis.cancel();
+
+  // create new utterance with all the properties
+  const text = wordList[count][0];
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.voice = window.speechSynthesis.getVoices().find(voice => voice.voiceURI === voiceInEl.value);
+  utterance.pitch = pitchInEl.value;
+  utterance.rate = rateInEl.value;
+  utterance.volume = volumeInEl.value;
+  
+  // speak that utterance
+  window.speechSynthesis.speak(utterance);
+}
+
+
+const setting = document.querySelector('.setting');
+const properties = document.querySelector('.properties');
+ 
+
+setting.addEventListener('click', () => {
+    
+    properties.style.display === 'none'? properties.style.display = 'block': properties.style.display = 'none';
+})
